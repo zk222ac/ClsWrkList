09184d38eb895649b7bf41f87fd9e1f809ad2926
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-10-12 12:25:24 +0200

    hi

--- ClsWrkList/Program.cs
+++ ClsWrkList/Program.cs
@@ -10,12 +10,96 @@ namespace ClsWrkList
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            //int[] array = new int[10];
+
+            //List<int> list = new List<int>();
+
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    list.Add(i);
+            //}
+
+            //int j = 0;
+            //foreach (var lis in list)
+            //{
+            //    Console.WriteLine($"List[{j}]- {lis}");
+            //    j++;
+            //}
+
+            //// Contain : check the existence of value inside list ...................................
+
+            //bool contain = list.Contains(55);
+            //Console.WriteLine(contain);//
+         
+            // list.Insert(4, 2000000000);
+
+            //Console.WriteLine("After Insertion.............");
+            //int k = 0;
+            //foreach (var lis in list)
+            //{
+            //    Console.WriteLine($"List[{k}]- {lis}");
+            //    k++;
+            //}
+
+
+            List<Student> list = new List<Student>();
+            // Declare the List object with object data items 
+           
+            List<Student> listStudent = new List<Student>()
+            {
+                new Student(12345, "Name1"),
+                new Student(56784, "Name2"),
+                new Student(34567, "Name3"),
+                new Student(1234, "Name4"),
+                new Student(5656, "Name5"),
+                new Student(76676, "Name6"),
+                new Student(342343, "Name7"),
+                new Student(8798, "Name8"),
+
+            };
+
+            // Display the data items 
+
+            foreach (var lis in listStudent)
+            {
+                Console.WriteLine(listStudent);
+                Console.WriteLine($"ID: {lis.ID} - Name: {lis.Name}");
+            }
+            //listStudent.Add( new Student(12345 , "Name1" ));
+            //listStudent.Add(new Student(34565, "Name2"));
+            //listStudent.Add(new Student(76684, "Name3"));
+
+            List<Student> newSearchList  = new List<Student>();
+            Console.WriteLine("Insert searching Id");
+            string searchId = Console.ReadLine();
+            while (searchId != null && searchId.ToUpper() != "STOP")
+            {
+                foreach (var lis in listStudent)
+                {
+                    Console.WriteLine("Search iterations.........");
+                    if (lis.ID.ToString() == searchId)
+                    {
+                        Console.WriteLine("Search is Found");
+                        newSearchList.Add(lis);
+                    }
+                }
+
+                Console.WriteLine("Do you want to search the Item again");
+                searchId = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("While Loop is terminated");
+
+            foreach (var searchFound in newSearchList)
+            {
+                Console.WriteLine("New search item found ");
+                Console.WriteLine($"Name for new search Id : {searchFound.Name}");
+            }
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
         }
     }
 }
---
+++ ClsWrkList/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsWrkList
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Student(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public int GetId()
+        {
+            return ID;
+        }
+       
+        public string GetName()
+        {
+            return Name;
+        }
+
+      
+
+
+
+    }
+}
